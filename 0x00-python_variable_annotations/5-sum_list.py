cff8bf9910fd2da666c4dea1bf5e94e3b0516d69
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-05 02:30:09 +0100

    "Added float conversion to sum_list function return value"

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Returns the sum of all elements
     in the input list."""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of all elements
     in the input list."""
     return sum(n for n in input_list)
