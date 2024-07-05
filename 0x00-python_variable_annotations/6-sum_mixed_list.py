58b6561cf27b21eef2cca7a2812f4a0de19f4595
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-05 02:35:02 +0100

    "Formatted function definition by adding a line break and indentation."

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,8 @@ import typing
     in the input list."""
 
 
-def sum_mixed_list(mxd_lst: list[typing.Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]
+                   ) -> float:
     """Returns the sum of all elements
     in the input list."""
     return sum(n for n in mxd_lst)
