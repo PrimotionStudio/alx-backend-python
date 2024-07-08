6bf076c36e63a383020b2d63ee66711d0df0573f
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-08 17:23:19 +0100

    "Added a blank line in 1-concurrent_coroutines.py"

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     This function waits for n random numbers between 0 and max_delay
