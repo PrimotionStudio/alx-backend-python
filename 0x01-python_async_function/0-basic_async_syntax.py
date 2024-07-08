f14e8ba829d9ff634e9e10a4c20b50632ce0592a
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-08 15:30:45 +0100

    "Added a new line to the wait_random function, assigning a new random value to the delay variable."

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,5 +11,6 @@ async def wait_random(max_delay: int | float = 10) -> int | float:
     This function waits for a random delay
     """
     delay: int | float = random.uniform(0, max_delay)
+    delay: int | float = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
