836f5108d5c6b684e50b7a79c3f14d1cac8956b3
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-08 17:51:57 +0100

    "Updated 4-tasks.py: changed shebang, added docstring, imported task_wait_random, and implemented task_wait_n function"

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function waits for n random numbers between 0 and max_delay
+    """
+    coroutines: [float] = [task_wait_random(max_delay) for _ in range(n)]
+    res: float = await asyncio.gather(*coroutines)
+    return sorted(res)
