672fd90823a6a87ecc28948f0dcaaea91715bbf6
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-08 17:37:00 +0100

    "Added shebang, docstring, and import statement; implemented measure_time function to measure execution time of wait_n function."

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+function that measures the total execution time
+for wait_n(n, max_delay), and returns total_time / ntime.
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    function that measures the total execution time
+    for wait_n(n, max_delay), and returns total_time / n.
+    """
+    start: float = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time: float = time.perf_counter() - start
+    return total_time / n
