601bf9c78509c8585d8662b7575ec50f00da80af
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-08 17:22:04 +0100

    "Added import statement for typing and updated type hint for wait_n function return value"

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
 This module contains the function
 """
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> [float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     This function waits for n random numbers between 0 and max_delay
     """
