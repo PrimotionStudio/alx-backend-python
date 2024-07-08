ee301a47af110e63c6ccd775bb51b2a7e30d6001
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-08 17:03:25 +0100

    "Refactor wait_n function to use asyncio.gather and return sorted list of delays"

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python3
-"""
-This module contains the function
-"""
+
+'''Task 1: Let's execute multiple coroutines
+at the same time with async
+'''
+
 import asyncio
+from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
-    """
-    This function waits for n random numbers between 0 and max_delay
-    """
-    # coroutines: [float] = [wait_random(max_delay) for _ in range(n)]
-    # return sorted(await asyncio.gather(*coroutines))
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    
-    # Gather all the results
-    results = await asyncio.gather(*tasks)
-    
-    # Insert the results in a way that avoids sorting the entire list
-    delays = []
-    for result in results:
-        # Find the position where to insert the result to maintain ascending order
-        i = 0
-        while i < len(delays) and delays[i] < result:
-            i += 1
-        delays.insert(i, result)
-    
-    return delays
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''return the list of all the delays (float values)
+    '''
+    waits = await asyncio.gather(
+        *list(map(lambda _: wait_random(max_delay), range(n)))
+    )
+
+    return sorted(waits)
