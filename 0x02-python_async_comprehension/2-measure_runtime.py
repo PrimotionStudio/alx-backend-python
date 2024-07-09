7f11b250cbc2b6b93ac8467dd883cdaae1cbab65
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-09 20:52:27 +0100

    "Refactored measure_runtime function to return float and simplified asyncio.gather call"

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,18 +5,14 @@ This module contains the function
 import asyncio
 import time
 
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     This function measures the time
     it takes to complete
     """
-    start = time.perf_counter()
-    result = await asyncio.gather(
-        async_comprehension(), async_comprehension(),
-        async_comprehension(), async_comprehension())
-    elapsed = time.perf_counter() - start
-    return elapsed
+    start: float = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start
