44515f52d31ea5082bf23caddb1c48b298521960
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-09 20:37:05 +0100

    "Added docstrings to script and function"

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """
+This script is used to generate
+a list of all the files in a directory
 """
 import asyncio
 import typing
@@ -9,4 +11,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
+    """
+    This function is used to generate
+    a list of all the files in a directory
+    """
     return [i async for i in async_generator()]
