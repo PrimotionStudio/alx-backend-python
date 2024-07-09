3516f7e70e1d8a231c1730805992355346ac06cf
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-09 20:34:05 +0100

    "Updated shebang line, added docstring, imports, and async function async_comprehension"

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+"""
+import asyncio
+import typing
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    return [i async for i in async_generator()]
