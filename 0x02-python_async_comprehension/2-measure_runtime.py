70d2f496789ef21b4d89e085d1aa95b2920d7f16
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-09 20:45:25 +0100

    "Updated 2-measure_runtime.py to import and call async_comprehension function, and modified shebang line. Also updated 1-async_comprehension.cpython-312.pyc binary file."

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function
+"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    This function measures the time
+    it takes to complete
+    """
+    start = time.perf_counter()
+    result = await asyncio.gather(
+        async_comprehension(), async_comprehension(),
+        async_comprehension(), async_comprehension())
+    elapsed = time.perf_counter() - start
+    return elapsed
