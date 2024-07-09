538e7496305cc229fa0822890d1fb07a5fdb160e
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-09 20:24:03 +0100

    "Moved await asyncio.sleep(1) to before yield statement in async_generator function."

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,5 +12,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     This function returns a random number between 0 and 10
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
