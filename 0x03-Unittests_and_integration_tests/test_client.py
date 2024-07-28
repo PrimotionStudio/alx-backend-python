52f282f0eb6b3d0b3258114baefad066085f4688
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-28 20:07:06 +0100

    "Added comments and tests to test_client.py, including a new test method test_public_repos_url."

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
+lorem ipsum dolor latom
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 
@@ -11,6 +12,7 @@ GithubOrgClient = __import__('client').GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
     """
+    lorem ipsum dolor latom
     """
     @parameterized.expand([
         ('google',),
@@ -19,8 +21,29 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
         """
+        lorem ipsum dolor latom
         """
         client = GithubOrgClient(org_name)
         client.org()
-        mock_get_json.return_value = {'key', 'value'}
+        mock_get_json.return_value = {'key': 'value'}
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+    @parameterized.expand([
+        (
+            'github',
+            {'repos_url': 'https://api.github.com'},
+        ),
+        (
+            'gitlab',
+            {'repos_url': 'https://api.gitlab.com'},
+        ),
+    ])
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, org_name, repo_url, mock_org):
+        """
+        lorem ipsum dolor latom
+        """
+        mock_org.return_value = repo_url
+        client = GithubOrgClient(org_name)
+        # print("=>", repo_url) 
+        self.assertEqual(client._public_repos_url, repo_url['repos_url'])
