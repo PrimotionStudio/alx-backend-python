2d44ed310ae371b6508dae872883bdd14e0a59f7
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-28 13:55:31 +0100

    "Removed test.py, updated test_utils.py, and modified __pycache__/test_utils.cpython-312.pyc"

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,6 +15,7 @@ from typing import (
 
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -85,3 +86,37 @@ class TestGetJson(unittest.TestCase):
         mock_requests.get.return_value = mock_response
 
         self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test the memoize function.
+    """
+    def test_memoize(self):
+        """
+        Test the memoize function.
+        """
+        class TestClass:
+            """
+            Test class.
+            """
+            def a_method(self):
+                """
+                Test method.
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """
+                Test property.
+                """
+                return self.a_method()
+
+        test_instance = TestClass()
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_a_method:
+            first_call = test_instance.a_property
+            second_call = test_instance.a_property
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mock_a_method.assert_called_once()
