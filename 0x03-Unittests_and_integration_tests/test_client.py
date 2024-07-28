1b974d08286ef64b729fd7b3bc15874f27e46753
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-28 20:38:51 +0100

    "Updated test_client.py: reformatted code, added new test_public_repos method, and modified existing tests"

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -26,7 +26,8 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         client.org()
         mock_get_json.return_value = {'key': 'value'}
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
 
     @parameterized.expand([
         (
@@ -45,5 +46,23 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         mock_org.return_value = repo_url
         client = GithubOrgClient(org_name)
-        # print("=>", repo_url) 
+        # print("=>", repo_url)
         self.assertEqual(client._public_repos_url, repo_url['repos_url'])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """
+        lorem ipsum
+        """
+        mock_repos_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+        mock_get_json.return_value = mock_repos_payload
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = 'https://api.github.com'
+            self.assertEqual(GithubOrgClient('github').public_repos(), [
+                             "repo1", "repo2", "repo3"])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once()
