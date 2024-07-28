fa607a1d0c80d08ff07b60bfb3b1e7eb6de7f415
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-28 15:40:31 +0100

    "Added unittests for GithubOrgClient class"

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+
+GithubOrgClient = __import__('client').GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    """
+    @parameterized.expand([
+        ('google',),
+        ('abc',)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """
+        """
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get_json.return_value = {'key', 'value'}
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
